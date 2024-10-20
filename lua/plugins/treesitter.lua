eb1e4d9dfd846e9335dbb74126a7e048e6f705d6
Csongor Bikki <bikkicsongor@proton.me>
2024-10-20 21:38:26 +0100

    attempt at java

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    opts = { ensure_installed = { "java" } },
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
